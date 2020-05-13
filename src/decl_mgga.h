512bb217b4c2c1c46a042ccf12b31c33ad78ded2
Miguel Marques <marques@tddft.org>
2020-05-13 13:58:16 +0200

    Ferromagnetic code removed from meta-GGAs.

--- src/decl_mgga.h
+++ src/decl_mgga.h
@@ -14,22 +14,14 @@ func_unpol(const xc_func_type *p, int order, const double *rho, const double *si
            MGGA_OUT_PARAMS_NO_EXC(double *));
 
 GPU_FUNCTION static inline void
-func_ferr(const xc_func_type *p, int order, const double *rho, const double *sigma, const double *lapl, const double *tau,
-          MGGA_OUT_PARAMS_NO_EXC(double *));
-
-GPU_FUNCTION static inline void
-func_pol(const xc_func_type *p, int order, const double *rho, const double *sigma, const double *lapl, const double *tau,
-         MGGA_OUT_PARAMS_NO_EXC(double *));
+func_pol  (const xc_func_type *p, int order, const double *rho, const double *sigma, const double *lapl, const double *tau
+           MGGA_OUT_PARAMS_NO_EXC(XC_COMMA double *, ));
 
 #else
 
 GPU_FUNCTION static inline void
 func_unpol(const xc_func_type *p, int order, const double *rho, const double *sigma, const double *lapl, const double *tau,
            double *zk, MGGA_OUT_PARAMS_NO_EXC(double *));
-
-GPU_FUNCTION static inline void
-func_ferr(const xc_func_type *p, int order, const double *rho, const double *sigma, const double *lapl, const double *tau,
-          double *zk, MGGA_OUT_PARAMS_NO_EXC(double *));
 
 GPU_FUNCTION static inline void
 func_pol(const xc_func_type *p, int order, const double *rho, const double *sigma, const double *lapl, const double *tau,
